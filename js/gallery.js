5032d1eb01966f8fb92333a33f9dab1d07a69916
dmytronayda <dima.nayda.1993@gmail.com>
2019-07-17 14:33:55 +0200

    Add changing main image functionality to gallery page.

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,16 @@
 function activateGallery() {
-    alert("Hello from the gallery file!");
-  }
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo img"); 
+    
+    thumbnails.forEach(function (thumbnail) {
+        thumbnail.addEventListener("click", function () {
+            // Set clicked image as main image. 
+            let newImageSource = thumbnail.dataset.largeVersion; 
+            // Set clicked image description for the main image.
+            let newImageAlt = thumbnail.dataset.description; 
+            mainImage.setAttribute("src", newImageSource); 
+            mainImage.setAttribute("alt", newImageAlt);
+            // Add border styling for current thumbnail
+        });
+    });
+}
