209d375ef6f8fed6e842d6673157bb3ad305080f
dmytronayda <dima.nayda.1993@gmail.com>
2019-07-18 14:09:32 +0200

    Add galery functionality in JS.

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,27 @@
 function activateGallery() {
     let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-    let mainImage = document.querySelector("#gallery-photo img"); 
-    
+    let mainImage = document.querySelector("#gallery-photo img");
+
     thumbnails.forEach(function (thumbnail) {
         thumbnail.addEventListener("click", function () {
             // Set clicked image as main image. 
-            let newImageSource = thumbnail.dataset.largeVersion; 
+            let newImageSource = thumbnail.dataset.largeVersion;
             // Set clicked image description for the main image.
-            let newImageAlt = thumbnail.dataset.description; 
-            mainImage.setAttribute("src", newImageSource); 
+            let newImageAlt = thumbnail.dataset.description;
+            mainImage.setAttribute("src", newImageSource);
             mainImage.setAttribute("alt", newImageAlt);
-            // Add border styling for current thumbnail
+
+            // Remove current class and add to the one clicked on 
+            let currentClass = document.querySelector(".current"); 
+            currentClass.classList.remove("current");
+
+            thumbnail.parentNode.classList.add("current"); 
+
+            //Change description
+            let title = document.querySelector("#gallery-info h3");
+            let description = document.querySelector("#gallery-info p");
+            title.innerHTML = thumbnail.dataset.title; 
+            description.innerHTML = thumbnail.dataset.description; 
         });
     });
 }
